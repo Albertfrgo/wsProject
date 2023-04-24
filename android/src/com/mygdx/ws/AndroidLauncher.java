7f0cacc826d450f1a154e27fd8b0386c652909ea
Albertfrgo <Albertfr2021@outlook.es>
2023-04-24 11:08:34 +0200

    Initial commit

---
+++ android/src/com/mygdx/ws/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.ws;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.ws.wsProject;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new wsProject(), config);
+	}
+}
